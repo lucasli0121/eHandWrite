bc6cbb57cea0c20e2f633188ff18edccb804e9f5
lucasli <lgqzlx@hotmail.com>
2022-04-05 22:29:43 +0800

    add files

---
+++ buildSrc/src/main/java/com/build/config/dependencies.kt
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.build.config
+
+object Versions {
+    const val ktlint = "0.44.0"
+    const val compileSdkVersion = 31
+    const val buildToolsVersion = "30.0.3"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 28
+    const val versionCode = 33
+    const val versionName = "1.1.3"
+    const val appId = "com.write"
+}
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.1.2"
+    const val jdkDesugar = "com.android.tools:desugar_jdk_libs:1.1.5"
+
+    const val junit = "junit:junit:4.+"
+
+    const val gradleBintrayPlugin = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.7.1"
+    const val androidMavenGradlePlugin = "com.github.dcendents:android-maven-gradle-plugin:2.1"
+    const val material3 = "com.google.android.material:material:1.6.0-alpha02"
+
+    object Accompanist {
+        const val version = "0.23.1"
+        const val insets = "com.google.accompanist:accompanist-insets:$version"
+    }
+
+    object Kotlin {
+        const val version = "1.6.10"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object Coroutines {
+        private const val version = "1.6.0"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+    }
+    object AndroidX {
+        const val appcompat = "androidx.appcompat:appcompat:1.4.1"
+        const val coreKtx = "androidx.core:core-ktx:1.7.0"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4"
+        const val annotation = "androidx.annotation:annotation:1.3.0"
+        object Activity {
+            const val activityCompose = "androidx.activity:activity-compose:1.4.0"
+        }
+
+        object Compose {
+            const val snapshot = ""
+            const val version = "1.1.1"
+
+            const val foundation = "androidx.compose.foundation:foundation:$version"
+            const val layout = "androidx.compose.foundation:foundation-layout:$version"
+            const val material = "androidx.compose.material:material:$version"
+            const val materialIconsExtended = "androidx.compose.material:material-icons-extended:$version"
+            const val runtime = "androidx.compose.runtime:runtime:$version"
+            const val runtimeLivedata = "androidx.compose.runtime:runtime-livedata:$version"
+            const val ui = "androidx.compose.ui:ui:$version"
+            const val tooling = "androidx.compose.ui:ui-tooling:$version"
+            const val toolingPreview = "androidx.compose.ui:ui-tooling-preview:$version"
+            const val test = "androidx.compose.ui:ui-test:$version"
+            const val uiTest = "androidx.compose.ui:ui-test-junit4:$version"
+            const val uiTestManifest = "androidx.compose.ui:ui-test-manifest:$version"
+            const val uiUtil = "androidx.compose.ui:ui-util:${version}"
+            const val viewBinding = "androidx.compose.ui:ui-viewbinding:$version"
+
+            object Material3 {
+                const val snapshot = ""
+                const val version = "1.0.0-alpha06"
+
+                const val material3 = "androidx.compose.material3:material3:$version"
+            }
+        }
+
+        object Navigation {
+            private const val version = "2.4.1"
+            const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
+            const val uiKtx = "androidx.navigation:navigation-ui-ktx:$version"
+        }
+
+        object Test {
+            private const val version = "1.4.0"
+            const val core = "androidx.test:core:$version"
+            const val rules = "androidx.test:rules:$version"
+
+            object Ext {
+                private const val version = "1.1.3"
+                const val junit = "androidx.test.ext:junit-ktx:$version"
+            }
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
+        }
+
+        object Lifecycle {
+            private const val version = "2.4.1"
+            const val extensions = "androidx.lifecycle:lifecycle-extensions:$version"
+            const val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
+            const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+            const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:$version"
+        }
+    }
+    object Http {
+        const val asyncHttp = "com.loopj.android:android-async-http:1.4.11"
+    }
+}
+
+object Urls {
+    const val composeSnapshotRepo = "https://androidx.dev/snapshots/builds/" +
+        "${Libs.AndroidX.Compose.snapshot}/artifacts/repository/"
+    const val composeMaterial3SnapshotRepo = "https://androidx.dev/snapshots/builds/" +
+            "${Libs.AndroidX.Compose.Material3.snapshot}/artifacts/repository/"
+    const val accompanistSnapshotRepo = "https://oss.sonatype.org/content/repositories/snapshots"
+}
